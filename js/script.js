e0b6c9abfd7e4eba6cb956d5c99b0e6cd83ef8b9
sebitch <sebastian.weber1993@aol.com>
2017-10-08 18:56:41 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*$(document).ready(function(){
+
+    $(".js-familie").click(function(){
+    $("html, body").animate({scrollTop:$("#familie-section").offset().top}, 1000);    
+    });
+
+    $(".js-galerie").click(function(){
+    $("html, body").animate({scrollTop:$("#galerie-section1").offset().top}, 1000);    
+    });
+
+    $(".js-kameras").click(function(){
+    $("html, body").animate({scrollTop:$("#kamera-section").offset().top}, 1000);    
+    });
+
+    $(".js-schule").click(function(){
+    $("html, body").animate({scrollTop:$("#schule-section").offset().top}, 1000);    
+    });
+    
+    $(".js-nachricht").click(function(){
+    $("html, body").animate({scrollTop:$(".nachricht-section").offset().top}, 1000);    
+    });
+    
+    $("#j").click(function(){
+    $("html, body").animate({scrollTop:$(".nachricht-section").offset().top}, 1000);    
+    });
+    
+});*/
